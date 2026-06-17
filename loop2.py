67af104e7269e804ae8dc87253e523da5d245756
shubhamsirse507-jpg <shubhamsirse507@gmail.com>
2026-06-17 09:43:06 +0530

    Week3

--- loop2.py
+++ loop2.py
@@ -1,20 +1,35 @@
 
 #1 - 100  -> %5 -> sum -> square
-i = 1
-sum = 0
-while i <= 100:
-    if i % 5 == 0:
-        sum += i
-    i += 1
+print("option 1 : for find sum of square")
+print("option 2 : for find table of any no")
+print("oprion 3 : for fing sum of given square")
+op=int(input("Enter option: "))
 
-square = sum ** 2
-for i in range(1, 101):
-    if i % 5 == 0:
-        sum +=i
-print("Sum:", sum)
-print("Square of the sum is:", square)
+if op == 1 :
 
+    i=int(input("Enter a no: "))
+    sum = 0
+    while i <= 100:
+            if i % 2 == 0:
+                sum += i
+            i += 1
+            print(i)
+
+elif op == 2:
+    #find sum of square
+    a = int(input("Enter "))
+    sum = 0
+    square = sum ** 2
+    for i in range(1, 101):
+        if i % 5 == 0:
+            sum +=i
+    print("Sum:", sum)
+    print("sum is:", square)
+elif op == 3:
 # 5 table 
-n= 5
-for i in range(1, 11):
-    print(n, "x" , i , "=" , n*i)
+
+    num=int(input("Enter a No to get table of No : "))
+    for i in range(1 , 11):
+        print(num,"x",i,num*i)
+        #O/p ex : n=5 & i = no btw 1 to 10 = n*i = 5*2 = 10
+        
--- loop3.py
+++ loop3.py
@@ -2,5 +2,6 @@ uno =int(input("Enter the no: "))
 eno =int(input("Enter the no:"))
 for i in range (uno , eno+1):
     if i %12 == 0 and i % 15 == 0 and i %19 == 0:
-        print(i)
-
+            print(i)
+    else:
+            print("Enter valid no")
