0cf12b2d116754e28ca3d3a88c61e314fc2ded8a
shubhamsirse507-jpg <shubhamsirse507@gmail.com>
2026-07-05 20:59:53 +0530

    Implement exception handling for dictionary access
    Added a new try-except block to handle exceptions when accessing dictionary keys.

--- Exception_hand.py
+++ Exception_hand.py
@@ -5,3 +5,14 @@ try:
     print("divi",a/b)
 except ArithmeticError as q:
     print("No",q)
+
+
+
+try:
+    a=("name:ram")
+    print(a["name"])
+    print(a["ram"])
+except Exception as e:
+    print(e)
+else :
+    print("end")
