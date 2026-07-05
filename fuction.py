a8a4545322741ad00995abd08a88a885d46c8768
shubhamsirse507-jpg <shubhamsirse507@gmail.com>
2026-07-06 00:54:01 +0530

    Add files via upload

---
+++ fuction.py
@@ -0,0 +1,15 @@
+def student():
+    print("hello")
+
+student()
+
+def student_age(age):
+    print("Age is :",age)
+
+Age=int(input("Enter Student age: "))
+
+student_age(Age)
+
+def add(a,b):
+    return a+b
+print("Sum ia : ",add(10,20))
